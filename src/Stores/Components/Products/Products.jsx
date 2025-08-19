de88e9fb82fee18a7af63376e3eb55fe3a6b1c3b
sudheer-ganta1 <sudheerganta.work@gmail.com>
2025-08-19 18:33:34 +0530

    commited chmages

--- src/Stores/Components/Products/Products.jsx
+++ src/Stores/Components/Products/Products.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Mobiles from "../Mobiles/Mobiles";
 import Computers from "../Computers/Computers";
 import Ac from "../AC/Ac";
-import WomenFashion from "../WomenFashion/WomenFashion";
+
+import WomenFashion from "../womensFashion/WomenFashion";
 import MenFashion from "../MenFashion/MenFashion";
 import Watches from "../Watches/Watches";
-import Furniture from "../Furniture/Furniture";
+import Furniture from "../Furniture/Furniture.JSX";
 
 const Products = () => {
   return (
